f77cbc73020435ab9afe0bdf767ec51e5c9c455d
shahhshr <shahshreya2911@gmail.com>
2025-03-13 15:17:47 -0400

    Update AddScheduleServiceController.java

--- src/com/zodiacgroup/controller/AddScheduleServiceController.java
+++ src/com/zodiacgroup/controller/AddScheduleServiceController.java
@@ -1,13 +1,41 @@
 package com.zodiacgroup.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import com.zodiacgroup.dao.ServiceDAO;
+import com.zodiacgroup.model.Service;
 
 public class AddScheduleServiceController {
-
-    
+    @FXML
+    private TextField serviceIdField;
 
     @FXML
-    public void initialize() {
-        
+    private TextField customerIdField;
+
+    @FXML
+    private TextField customerNameField;
+
+    @FXML
+    private TextField vehicleNameField;
+
+    @FXML
+    private TextField serviceDetailField;
+
+    @FXML
+    private TextField dateField;
+
+    @FXML
+    private TextField timeField;
+
+    @FXML
+    private TextField mechanicField;
+
+    private ServiceDAO serviceDAO;
+
+    public AddScheduleServiceController() {
+        serviceDAO = new ServiceDAO();
     }
+
+    
 }
