a92c4fb884efb1f7a9c16b9961b80eae7b108946
shahhshr <shahshreya2911@gmail.com>
2025-03-13 14:37:28 -0400

    Update AppointmentController.java

--- src/com/zodiacgroup/controller/AppointmentController.java
+++ src/com/zodiacgroup/controller/AppointmentController.java
@@ -1,13 +1,29 @@
 package com.zodiacgroup.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import com.zodiacgroup.dao.AppointmentDAO;
+import com.zodiacgroup.model.Appointment;
 
 public class AppointmentController {
-
-    
+	 @FXML
+	 private TextField AppointmentIdField;
+	   
+	@FXML
+    private TextField customerIdField;
 
     @FXML
-    public void initialize() {
-        
+    private TextField customerNameField;
+
+    @FXML
+    private TextField vehicleNameField;
+
+    private AppointmentDAO appointmentDAO;
+
+    public AppointmentController() {
+        appointmentDAO = new AppointmentDAO();
     }
+
+  
 }
