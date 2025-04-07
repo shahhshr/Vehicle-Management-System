fbc0870e2e2376817facde014d12fc0a0f03670f
Poorva Patel <118352102+Poorva039@users.noreply.github.com>
2025-04-07 00:55:22 -0400

    Add new utils files

--- src/com/zodiacgroup/util/HibernateUtil.java
+++ src/com/zodiacgroup/util/HibernateUtil.java
@@ -1,24 +1,37 @@
 package com.zodiacgroup.util;
- 
+
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
- 
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
- 
+
     private static SessionFactory buildSessionFactory() {
         try {
-            return new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-        	System.err.println("Initial SessionFactory creation failed." + ex);
+            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml")
+                .build();
+
+            Metadata metadata = new MetadataSources(standardRegistry)
+                .addAnnotatedClass(com.zodiacgroup.model.Customer.class)
+                .addAnnotatedClass(com.zodiacgroup.model.Vehicle.class)
+                .getMetadataBuilder()
+                .build();
+
+            return metadata.getSessionFactoryBuilder().build();
+        } catch (Exception ex) {
+            System.err.println("Initial SessionFactory creation failed: " + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
- 
+
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
-    
+
     public static void shutdown() {
         if (sessionFactory != null) {
             sessionFactory.close();
